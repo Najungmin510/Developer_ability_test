1f5a7506a70b31a976ab5f689ce3c07bfb02a264
NaJungMin <wjdalszbxl98@naver.com>
2023-05-03 18:00:02 +0900

    Start of development

---
+++ app/src/main/java/src/Login_view.kt
@@ -0,0 +1,12 @@
+package src
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.developer_ability_test.R
+
+class Login_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_view)
+    }
+}
